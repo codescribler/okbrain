2c69c587625ace1f9583e2a1d6feeebab5a24f44
Daniel Whittaker <daniel@dreamfree.co.uk>
2014-08-15 13:19:09 +0100

    initial commit

---
+++ src/okbrain/okbrain/Modules/IndexModule.cs
@@ -0,0 +1,15 @@
+﻿using Nancy;
+
+namespace okbrain.Modules
+{
+    public class IndexModule : NancyModule
+    {
+        public IndexModule()
+        {
+            Get["/"] = parameters =>
+            {
+                return View["index"];
+            };
+        }
+    }
+}
