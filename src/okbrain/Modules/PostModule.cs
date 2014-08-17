97735fb3442adbe6a9d223ead70ca90f151a1f4e
Daniel Whittaker <daniel@dreamfree.co.uk>
2014-08-17 21:38:02 +0100

    adding forms

---
+++ src/okbrain/Modules/PostModule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace okbrain.Modules
+{
+    public class PostModule : BaseModule
+    {
+        public PostModule()
+        {
+            Get["/blog/posts/new"] = parameters =>
+                                         {
+                                             return View["newpost"];
+                                         };
+        }
+    }
+}
