15b0ad705b14cd1a99b4365456f912ff7289d71c
Daniel Whittaker <daniel@dreamfree.co.uk>
2014-08-18 08:56:54 +0100

    start for form submision

---
+++ src/okbrain/Extensions/Class1.cs
@@ -0,0 +1,23 @@
+﻿using Nancy;
+using Nancy.Responses.Negotiation;
+
+
+public static class NegotiateExtensions
+{
+    public static Negotiator ForJson(this Negotiator negotiator, object model)
+    {
+        return negotiator.WithMediaRangeModel(MediaRange.FromString("application/json"), model);
+    }
+
+    public static Negotiator ForXml(this Negotiator negotiator, object model)
+    {
+        return negotiator.WithMediaRangeModel(MediaRange.FromString("application/xml"), model);
+    }
+
+    public static Negotiator OrPartial(this Negotiator negotiator, object model)
+    {
+        return negotiator.ForJson(model).ForXml(model);
+    }
+}
+
+
--- src/okbrain/Modules/IndexModule.cs
+++ src/okbrain/Modules/IndexModule.cs
@@ -1,4 +1,5 @@
 ﻿using Nancy;
+using Nancy.ModelBinding;
 
 namespace okbrain.Modules
 {
@@ -7,9 +8,37 @@ namespace okbrain.Modules
         public IndexModule()
         {
             Get["/"] = parameters =>
-            {
-                return View["index"];
-            };
+                           {
+                               return View["index"];
+                           };
+
+            Post["/"] = parameters =>
+                            {
+                                string email = Request.Form.email;
+                                string real = Request.Form.real;
+
+                                var cr = new CommonResponse {Success = true, Data = null, Message = "Subscribed"};
+
+                                if (!string.IsNullOrEmpty(real))
+                                {
+                                    cr.Success = false;
+                                    cr.Data = null;
+                                    cr.Message = "Bots - go away!";
+                                }
+
+                                return Negotiate.WithView("index")
+                                                .WithModel(cr)
+                                                .OrPartial(cr);
+                            };
         }
     }
+
+    public class CommonResponse
+    {
+        public bool Success { get; set; }
+        public dynamic Data { get; set; }
+        public string Message { get; set; }
+    }
+
+   
 }
