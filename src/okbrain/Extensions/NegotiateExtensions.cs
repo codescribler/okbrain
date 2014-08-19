5bc98d8639bf4211f78817deda98f722de5818b2
Daniel Whittaker <daniel@dreamfree.co.uk>
2014-08-19 14:49:11 +0100

    Removed obsolete usage of media

--- src/okbrain/Extensions/NegotiateExtensions.cs
+++ src/okbrain/Extensions/NegotiateExtensions.cs
@@ -6,12 +6,12 @@ public static class NegotiateExtensions
 {
     public static Negotiator ForJson(this Negotiator negotiator, object model)
     {
-        return negotiator.WithMediaRangeModel(MediaRange.FromString("application/json"), model);
+        return negotiator.WithMediaRangeModel(new MediaRange("application/json"), model);
     }
 
     public static Negotiator ForXml(this Negotiator negotiator, object model)
     {
-        return negotiator.WithMediaRangeModel(MediaRange.FromString("application/xml"), model);
+        return negotiator.WithMediaRangeModel(new MediaRange("application/xml"), model);
     }
 
     public static Negotiator OrPartial(this Negotiator negotiator, object model)
