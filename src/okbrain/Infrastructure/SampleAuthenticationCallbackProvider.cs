d4b8974e8486a7be17662c2c94e574646bd81f6c
Daniel Whittaker <daniel@dreamfree.co.uk>
2014-08-27 10:30:35 +0100

    Added simple authentication callback

---
+++ src/okbrain/Infrastructure/SampleAuthenticationCallbackProvider.cs
@@ -0,0 +1,25 @@
+﻿
+
+namespace okbrain.Infrastructure
+{
+    using Models;
+    using Nancy;
+    using Nancy.SimpleAuthentication;
+
+    public class SampleAuthenticationCallbackProvider : IAuthenticationCallbackProvider
+    {
+        public dynamic Process(NancyModule nancyModule, AuthenticateCallbackData model)
+        {
+            return nancyModule.View["AuthenticateCallback", model];
+        }
+
+        public dynamic OnRedirectToAuthenticationProviderError(NancyModule nancyModule, string errorMessage)
+        {
+            var model = new {
+                ErrorMessage = errorMessage
+            };
+
+            return nancyModule.View["index", model];
+        }
+    }
+}
